43c9962d071f0cf406cd8d4062f4861d5cea1849
gamer4maker <gamer4maker@protonmail.com>
2016-03-26 23:38:33 +1100

    Outside Additions
    Also includes some quick fixes to ammo/health balance and enemy Ambush
    tags.
    
    There's also a sweet Shambler battle, quite intense.

--- dev/maps/compile.bat
+++ dev/maps/compile.bat
@@ -5,24 +5,24 @@ cd D:\Games\GOG\Quake\dev\maps
 
 
 echo Copying Files...
-copy D:\Games\GOG\Quake\dev\maps\quoth1a.map D:\Games\GOG\Quake\dev\maps
+copy D:\Games\GOG\Quake\dev\maps\quoth1b.map D:\Games\GOG\Quake\dev\maps
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-D:\Games\GOG\Quake\tools\bin\qbsp.exe quoth1a
+D:\Games\GOG\Quake\tools\bin\qbsp.exe quoth1b
 
 echo --------------VIS---------------
-D:\Games\GOG\Quake\tools\bin\vis.exe quoth1a
+D:\Games\GOG\Quake\tools\bin\vis.exe quoth1b
 
 echo -------------LIGHT--------------
-D:\Games\GOG\Quake\tools\bin\light.exe quoth1a
+D:\Games\GOG\Quake\tools\bin\light.exe quoth1b
 
-copy quoth1a.bsp D:\Games\GOG\Quake\id1\maps
-copy quoth1a.pts D:\Games\GOG\Quake\id1\maps
-copy quoth1a.lit D:\Games\GOG\Quake\id1\maps
+copy quoth1b.bsp D:\Games\GOG\Quake\id1\maps
+copy quoth1b.pts D:\Games\GOG\Quake\id1\maps
+copy quoth1b.lit D:\Games\GOG\Quake\id1\maps
 cd\
 cd D:\Games\GOG\Quake
-quakespasm  -hipnotic -game quoth +map quoth1a
+quakespasm  -hipnotic -game quoth +map quoth1b
