0e209794be36a195549c95d0f28c832bd151e56d
gamer4maker <gamer4maker@protonmail.com>
2016-03-26 17:47:52 +1100

    Gate Animation
    Bug: Door message plays on button.

--- dev/maps/compile.bat
+++ dev/maps/compile.bat
@@ -5,24 +5,24 @@ cd D:\Games\GOG\Quake\dev\maps
 
 
 echo Copying Files...
-copy D:\Games\GOG\Quake\dev\maps\quoth1.map D:\Games\GOG\Quake\dev\maps
+copy D:\Games\GOG\Quake\dev\maps\quoth1a.map D:\Games\GOG\Quake\dev\maps
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-D:\Games\GOG\Quake\tools\bin\qbsp.exe quoth1
+D:\Games\GOG\Quake\tools\bin\qbsp.exe quoth1a
 
 echo --------------VIS---------------
-D:\Games\GOG\Quake\tools\bin\vis.exe quoth1
+D:\Games\GOG\Quake\tools\bin\vis.exe quoth1a
 
 echo -------------LIGHT--------------
-D:\Games\GOG\Quake\tools\bin\light.exe quoth1
+D:\Games\GOG\Quake\tools\bin\light.exe quoth1a
 
-copy quoth1.bsp D:\Games\GOG\Quake\id1\maps
-copy quoth1.pts D:\Games\GOG\Quake\id1\maps
-copy quoth1.lit D:\Games\GOG\Quake\id1\maps
+copy quoth1a.bsp D:\Games\GOG\Quake\id1\maps
+copy quoth1a.pts D:\Games\GOG\Quake\id1\maps
+copy quoth1a.lit D:\Games\GOG\Quake\id1\maps
 cd\
 cd D:\Games\GOG\Quake
-quakespasm  -hipnotic -game quoth +map quoth1
+quakespasm  -hipnotic -game quoth +map quoth1a
