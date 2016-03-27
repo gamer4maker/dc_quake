35867a4c3311c0f64e080d32486858ebc9edd177
gamer4maker <gamer4maker@protonmail.com>
2016-03-27 22:14:49 +1100

    Replaced light with arrows
    Much better way to direct the player.

--- dev/maps/compile.bat
+++ dev/maps/compile.bat
@@ -5,24 +5,24 @@ cd D:\Games\GOG\Quake\dev\maps
 
 
 echo Copying Files...
-copy D:\Games\GOG\Quake\dev\maps\quoth-test.map D:\Games\GOG\Quake\dev\maps
+copy D:\Games\GOG\Quake\dev\maps\quoth1c.map D:\Games\GOG\Quake\dev\maps
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-D:\Games\GOG\Quake\tools\bin\qbsp.exe quoth-test
+D:\Games\GOG\Quake\tools\bin\qbsp.exe quoth1c
 
 echo --------------VIS---------------
-D:\Games\GOG\Quake\tools\bin\vis.exe quoth-test
+D:\Games\GOG\Quake\tools\bin\vis.exe quoth1c
 
 echo -------------LIGHT--------------
-D:\Games\GOG\Quake\tools\bin\light.exe quoth-test
+D:\Games\GOG\Quake\tools\bin\light.exe quoth1c
 
-copy quoth-test.bsp D:\Games\GOG\Quake\id1\maps
-copy quoth-test.pts D:\Games\GOG\Quake\id1\maps
-copy quoth-test.lit D:\Games\GOG\Quake\id1\maps
+copy quoth1c.bsp D:\Games\GOG\Quake\id1\maps
+copy quoth1c.pts D:\Games\GOG\Quake\id1\maps
+copy quoth1c.lit D:\Games\GOG\Quake\id1\maps
 cd\
 cd D:\Games\GOG\Quake
-quakespasm  -hipnotic -game quoth +map quoth-test
+quakespasm  -hipnotic -game quoth +map quoth1c
